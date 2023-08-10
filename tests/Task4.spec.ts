0ad7efea1b8a6ac3fc43c8adf5ef966ea16c623d
Alireza Tabatabaeian <alireza2541@aut.ac.ir>
2023-08-11 01:22:57 +0330

    just a commit

--- tests/Task4.spec.ts
+++ tests/Task4.spec.ts
@@ -32,13 +32,13 @@ describe('Task4', () => {
     });
 
     it('should deploy', async () => {
-        let shift = 1
-        // let text = "_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_"
-        let text = "AB"
+        let shift = -27
+        let text = "_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_"
+        // let text = "ABC"
         let enc_text = await task4.get_caesar_cipher_encrypt(shift, text)
 
-        console.log("Outpu Character is:", enc_text)
+        console.log("Enc Character is:", enc_text)
 
-        console.log("Outpu Character is:", await task4.get_caesar_cipher_decrypt(shift, "BC"))
+        console.log("Dec Character is:", await task4.get_caesar_cipher_decrypt(shift, enc_text))
     });
 });
