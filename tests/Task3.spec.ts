5d7bd1b0cb9cfa551387765d4687cec0665dd9ec
Alireza Tabatabaeian <alireza2541@aut.ac.ir>
2023-08-17 03:05:42 +0330

    just a commit

---
+++ tests/Task3.spec.ts
@@ -0,0 +1,37 @@
+import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { Cell, toNano } from 'ton-core';
+import { Task3 } from '../wrappers/Task3';
+import '@ton-community/test-utils';
+import { compile } from '@ton-community/blueprint';
+
+describe('Task3', () => {
+    let code: Cell;
+
+    beforeAll(async () => {
+        code = await compile('Task3');
+    });
+
+    let blockchain: Blockchain;
+    let task3: SandboxContract<Task3>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        task3 = blockchain.openContract(Task3.createFromConfig({}, code));
+
+        const deployer = await blockchain.treasury('deployer');
+
+        const deployResult = await task3.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: task3.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it('should deploy', async () => {
+        console.log(await task3.get_find_and_replace(2, 4))
+    });
+});
